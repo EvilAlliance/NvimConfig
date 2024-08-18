428d797f3814f46a607a13d1164da52968ca33f7
Pedro Chialanza <chialanza17@gmail.com>
2024-08-18 18:58:16 -0300

    Float terminal

---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'numToStr/FTerm.nvim',
+    config = function()
+      require('FTerm').setup {
+        border = 'double',
+        dimensions = {
+          height = 0.9,
+          width = 0.9,
+        },
+      }
+      vim.keymap.set('n', '<C-t>', '<CMD>lua require("FTerm").toggle()<CR>')
+      vim.keymap.set('t', '<C-q>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+      vim.keymap.set('t', '<C-t>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+    end,
+  },
+}
