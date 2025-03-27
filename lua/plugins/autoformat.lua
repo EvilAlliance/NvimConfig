b62d4fa36b540e60a47593a0adaf5f429b34c437
EvilAlliance <chialanza17@gmail.com>
2025-03-27 10:59:55 -0300

    Mark new instaed of harpoon

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -1,146 +1,146 @@
 return {
-  { -- Autoformat
-    'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
-    keys = {
-      {
-        '<leader>f',
-        function()
-          require('conform').format { async = true, lsp_fallback = true }
+    { -- Autoformat
+        'stevearc/conform.nvim',
+        event = { 'BufWritePre' },
+        cmd = { 'ConformInfo' },
+        keys = {
+            {
+                '<leader>f',
+                function()
+                    require('conform').format { async = true, lsp_fallback = true }
+                end,
+                mode = '',
+                desc = '[F]ormat buffer',
+            },
+        },
+        opts = {
+            notify_on_error = false,
+            format_on_save = function(bufnr)
+                -- Disable "format_on_save lsp_fallback" for languages that don't
+                -- have a well standardized coding style. You can add additional
+                -- languages here or re-enable it for the disabled ones.
+                local disable_filetypes = { c = true, cpp = true }
+                return {
+                    timeout_ms = 500,
+                    lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+                }
+            end,
+            formatters_by_ft = {
+                lua = { 'stylua' },
+                -- Conform can also run multiple formatters sequentially
+                -- python = { "isort", "black" },
+                --
+                -- You can use 'stop_after_first' to run the first available formatter from the list
+                -- javascript = { "prettierd", "prettier", stop_after_first = true },
+            },
+        },
+    },
+    { -- Highlight, edit, and navigate code
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+        opts = {
+            ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+            -- Autoinstall languages that are not installed
+            auto_install = true,
+            highlight = {
+                enable = true,
+                -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+                --  If you are experiencing weird indenting issues, add the language to
+                --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+                additional_vim_regex_highlighting = { 'ruby' },
+            },
+            indent = { enable = true, disable = { 'ruby' } },
+        },
+        config = function(_, opts)
+            -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+
+            ---@diagnostic disable-next-line: missing-fields
+            require('nvim-treesitter.configs').setup(opts)
+
+            -- There are additional nvim-treesitter modules that you can use to interact
+            -- with nvim-treesitter. You should go explore a few and see what interests you:
+            --
+            --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+            --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+            --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
         end,
-        mode = '',
-        desc = '[F]ormat buffer',
-      },
     },
-    opts = {
-      notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        return {
-          timeout_ms = 500,
-          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-        }
-      end,
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
-      },
+    {
+        'nmac427/guess-indent.nvim',
+        opts = {
+            auto_cmd = true, -- Set to false to disable automatic execution
+            override_editorconfig = false, -- Set to true to override settings set by .editorconfig
+            filetype_exclude = { -- A list of filetypes for which the auto command gets disabled
+                'netrw',
+                'tutor',
+            },
+            buftype_exclude = { -- A list of buffer types for which the auto command gets disabled
+                'help',
+                'nofile',
+                'terminal',
+                'prompt',
+            },
+            on_tab_options = { -- A table of vim options when tabs are detected
+                ['expandtab'] = false,
+            },
+            on_space_options = { -- A table of vim options when spaces are detected
+                ['expandtab'] = true,
+                ['tabstop'] = 'detected', -- If the option value is 'detected', The value is set to the automatically detected indent size.
+                ['softtabstop'] = 'detected',
+                ['shiftwidth'] = 'detected',
+            },
+        },
     },
-  },
-  { -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
-    config = function(_, opts)
-      -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+    { -- Linting
+        'mfussenegger/nvim-lint',
+        event = { 'BufReadPre', 'BufNewFile' },
+        config = function()
+            local lint = require 'lint'
+            lint.linters_by_ft = {
+                markdown = { 'markdownlint' },
+            }
 
-      ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup(opts)
+            -- To allow other plugins to add linters to require('lint').linters_by_ft,
+            -- instead set linters_by_ft like this:
+            -- lint.linters_by_ft = lint.linters_by_ft or {}
+            -- lint.linters_by_ft['markdown'] = { 'markdownlint' }
+            --
+            -- However, note that this will enable a set of default linters,
+            -- which will cause errors unless these tools are available:
+            -- {
+            --   clojure = { "clj-kondo" },
+            --   dockerfile = { "hadolint" },
+            --   inko = { "inko" },
+            --   janet = { "janet" },
+            --   json = { "jsonlint" },
+            --   markdown = { "vale" },
+            --   rst = { "vale" },
+            --   ruby = { "ruby" },
+            --   terraform = { "tflint" },
+            --   text = { "vale" }
+            -- }
+            --
+            -- You can disable the default linters by setting their filetypes to nil:
+            -- lint.linters_by_ft['clojure'] = nil
+            -- lint.linters_by_ft['dockerfile'] = nil
+            -- lint.linters_by_ft['inko'] = nil
+            -- lint.linters_by_ft['janet'] = nil
+            -- lint.linters_by_ft['json'] = nil
+            -- lint.linters_by_ft['markdown'] = nil
+            -- lint.linters_by_ft['rst'] = nil
+            -- lint.linters_by_ft['ruby'] = nil
+            -- lint.linters_by_ft['terraform'] = nil
+            -- lint.linters_by_ft['text'] = nil
 
-      -- There are additional nvim-treesitter modules that you can use to interact
-      -- with nvim-treesitter. You should go explore a few and see what interests you:
-      --
-      --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-      --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-      --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-    end,
-  },
-  {
-    'nmac427/guess-indent.nvim',
-    opts = {
-      auto_cmd = true, -- Set to false to disable automatic execution
-      override_editorconfig = false, -- Set to true to override settings set by .editorconfig
-      filetype_exclude = { -- A list of filetypes for which the auto command gets disabled
-        'netrw',
-        'tutor',
-      },
-      buftype_exclude = { -- A list of buffer types for which the auto command gets disabled
-        'help',
-        'nofile',
-        'terminal',
-        'prompt',
-      },
-      on_tab_options = { -- A table of vim options when tabs are detected
-        ['expandtab'] = false,
-      },
-      on_space_options = { -- A table of vim options when spaces are detected
-        ['expandtab'] = true,
-        ['tabstop'] = 'detected', -- If the option value is 'detected', The value is set to the automatically detected indent size.
-        ['softtabstop'] = 'detected',
-        ['shiftwidth'] = 'detected',
-      },
-    },
-  },
-  { -- Linting
-    'mfussenegger/nvim-lint',
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-      local lint = require 'lint'
-      lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
-      }
-
-      -- To allow other plugins to add linters to require('lint').linters_by_ft,
-      -- instead set linters_by_ft like this:
-      -- lint.linters_by_ft = lint.linters_by_ft or {}
-      -- lint.linters_by_ft['markdown'] = { 'markdownlint' }
-      --
-      -- However, note that this will enable a set of default linters,
-      -- which will cause errors unless these tools are available:
-      -- {
-      --   clojure = { "clj-kondo" },
-      --   dockerfile = { "hadolint" },
-      --   inko = { "inko" },
-      --   janet = { "janet" },
-      --   json = { "jsonlint" },
-      --   markdown = { "vale" },
-      --   rst = { "vale" },
-      --   ruby = { "ruby" },
-      --   terraform = { "tflint" },
-      --   text = { "vale" }
-      -- }
-      --
-      -- You can disable the default linters by setting their filetypes to nil:
-      -- lint.linters_by_ft['clojure'] = nil
-      -- lint.linters_by_ft['dockerfile'] = nil
-      -- lint.linters_by_ft['inko'] = nil
-      -- lint.linters_by_ft['janet'] = nil
-      -- lint.linters_by_ft['json'] = nil
-      -- lint.linters_by_ft['markdown'] = nil
-      -- lint.linters_by_ft['rst'] = nil
-      -- lint.linters_by_ft['ruby'] = nil
-      -- lint.linters_by_ft['terraform'] = nil
-      -- lint.linters_by_ft['text'] = nil
-
-      -- Create autocommand which carries out the actual linting
-      -- on the specified events.
-      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
-        group = lint_augroup,
-        callback = function()
-          lint.try_lint()
+            -- Create autocommand which carries out the actual linting
+            -- on the specified events.
+            local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+            vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+                group = lint_augroup,
+                callback = function()
+                    lint.try_lint()
+                end,
+            })
         end,
-      })
-    end,
-  },
+    },
 }
