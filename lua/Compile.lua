a153b65245bfee088f741ef53babff4b3c5c3b25
EvilAlliance <chialanza17@gmail.com>
2025-04-24 10:56:17 -0300

    better compile

--- lua/Compile.lua
+++ lua/Compile.lua
@@ -1,4 +1,48 @@
--- Auto-open quickfix window if errors exist after make/grep/etc.
+local json = vim.fn.json_encode
+local json_decode = vim.fn.json_decode
+
+-- Path to store per-project makeprg values
+local store_path = vim.fn.stdpath 'data' .. '/project_makeprg.json'
+local makeprg_store = {}
+
+-- Load existing saved commands
+local function load_store()
+    local f = io.open(store_path, 'r')
+    if f then
+        makeprg_store = json_decode(f:read '*a') or {}
+        f:close()
+    end
+end
+
+-- Save to file
+local function save_store()
+    local f = io.open(store_path, 'w')
+    if f then
+        f:write(json(makeprg_store))
+        f:close()
+    end
+end
+
+-- Identify project by cwd
+local function get_project_key()
+    return vim.fn.getcwd()
+end
+
+-- Auto-load on startup
+load_store()
+
+-- Restore makeprg on file open
+vim.api.nvim_create_autocmd('BufEnter', {
+    callback = function()
+        local key = get_project_key()
+        local saved = makeprg_store[key]
+        if saved then
+            vim.o.makeprg = saved
+        end
+    end,
+})
+
+-- Auto-open quickfix window if errors exist after :make
 vim.api.nvim_create_autocmd('QuickFixCmdPost', {
     pattern = { 'make' },
     callback = function()
@@ -10,36 +54,40 @@ vim.api.nvim_create_autocmd('QuickFixCmdPost', {
     end,
 })
 
+-- Command to change makeprg and persist it
 vim.api.nvim_create_user_command('ChangeCommand', function()
-    local input = vim.fn.input('Set makeprg: ', vim.o.makeprg.sub(vim.o.makeprg, 0, vim.o.makeprg.len(vim.o.makeprg) - 2))
+    local current = vim.o.makeprg
+    local input = vim.fn.input('Set makeprg: ', current)
 
     if input == '' then
         print 'Cancelled.'
         return
     end
+
     vim.o.makeprg = input
+    makeprg_store[get_project_key()] = input
+    save_store()
 end, {})
 
+-- Run make, prompting for makeprg if not set
 vim.api.nvim_create_user_command('RunCommand', function()
-    local makeprg = vim.o.makeprg
-
-    -- If makeprg is not set or empty
-    if makeprg == '' or makeprg == 'make' then
-        -- Ask the user for a command
+    if vim.o.makeprg == '' or vim.o.makeprg == 'make' then
         local input = vim.fn.input 'Set makeprg: '
         if input == '' then
             print 'Cancelled.'
             return
         end
         vim.o.makeprg = input
+        makeprg_store[get_project_key()] = input
+        save_store()
     end
 
-    -- Run :make
-    vim.cmd 'make'
+    vim.cmd 'make!'
 end, {})
 
-vim.keymap.set({ 'n', 'i', 'v' }, '<C-i>', ':RunCommand<enter>', { desc = 'Run Command' })
-vim.keymap.set({ 'n' }, ';', ':ChangeCommand<enter>', { desc = 'Change Command' })
-
+-- Keymaps
+vim.keymap.set({ 'n', 'i', 'v' }, '<C-i>', ':RunCommand<CR>', { desc = 'Run Command' })
+vim.keymap.set('n', ';', ':ChangeCommand<CR>', { desc = 'Change Command' })
 vim.keymap.set('n', ']e', '<Cmd>try | cnext | catch | cfirst | catch | endtry<CR><CR>', { desc = 'Next Error' })
 vim.keymap.set('n', '[e', '<Cmd>try | cprevious | catch | clast | catch | endtry<CR><CR>', { desc = 'Prev Error' })
+vim.keymap.set('n', '<M-i>', '<Cmd>Trouble qflist close<CR>', { desc = 'Close Trouble QuickFix' })
--- lua/plugins/basic.lua
+++ lua/plugins/basic.lua
@@ -109,4 +109,35 @@ return {
             },
         },
     },
+    {
+        'tzachar/highlight-undo.nvim',
+        opts = {
+            hlgroup = 'HighlightUndo',
+            duration = 300,
+            pattern = { '*' },
+            ignored_filetypes = { 'neo-tree', 'fugitive', 'TelescopePrompt', 'mason', 'lazy' },
+            -- ignore_cb is in comma as there is a default implementation. Setting
+            -- to nil will mean no default os called.
+            -- ignore_cb = nil,
+        },
+    },
+    {
+        'romgrk/nvim-treesitter-context',
+        config = function()
+            require('treesitter-context').setup {
+                enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+                throttle = true, -- Throttles plugin updates (may improve performance)
+                max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+                patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+                    -- For all filetypes
+                    -- Note that setting an entry here replaces all other patterns for this entry.
+                    -- By setting the 'default' entry below, you can control which nodes you want to
+                    -- appear in the context window.
+                    default = {
+                        '*',
+                    },
+                },
+            }
+        end,
+    },
 }
